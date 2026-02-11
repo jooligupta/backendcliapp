1899ce1d3b6e2936ad262245a14e00aa2c55a52a
jooligupta <80266691+jooligupta@users.noreply.github.com>
2026-02-11 10:51:52 -0800

    Changes category controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -25,7 +25,44 @@ const getCategories = async (req, res) => {
     }
 };
 
+
+
+const updateCategory = async (req, res) => {
+    try {
+        const { name } = req.body;
+        const category = await Category.findById(req.params.id);
+
+        if (category) {
+            category.name = name || category.name;
+            category.slug = name ? name.toLowerCase().replace(/ /g, "-") : category.slug;
+
+            const updatedCategory = await category.save();
+            res.json(updatedCategory);
+        } else {
+            res.status(404).json({ message: "Category not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+const deleteCategory = async (req, res) => {
+    try {
+        const category = await Category.findById(req.params.id);
+
+        if (category) {
+            await category.deleteOne();
+            res.json({ message: "Category removed" });
+        } else {
+            res.status(404).json({ message: "Category not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
 module.exports = {
     createCategory,
-    getCategories
+    getCategories,
+    updateCategory,
+    deleteCategory
 };
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const{ createCategory, getCategories } = require("../controllers/categoryController.js");
-const {protect} = require('../middleware/authMiddleware.js');
-const{isAdmin} = require('../middleware/adminMiddleware.js');
+const { createCategory, getCategories, updateCategory, deleteCategory } = require("../controllers/categoryController.js");
+const { protect } = require('../middleware/authMiddleware.js');
+const { isAdmin } = require('../middleware/adminMiddleware.js');
 
 
 const router = express.Router();
 
-router.post("/",protect,isAdmin, createCategory);
-router.get("/",protect, getCategories);
+router.post("/", protect, isAdmin, createCategory);
+router.get("/", protect, getCategories);
+router.put("/:id", protect, isAdmin, updateCategory);
+router.delete("/:id", protect, isAdmin, deleteCategory);
 
 module.exports = router;
